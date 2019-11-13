1d2b3a5a0fe9264e3c6d2d41d2395dfd9a9a1bbe
nvando <nikievandorst@gmail.com>
2019-11-13 20:31:52 +1100

    Second version, includes invalid tags replacement

--- wordfinder.py
+++ wordfinder.py
@@ -12,11 +12,15 @@ ctx.verify_mode = ssl.CERT_NONE
 #count the frequency of the word "Guild"
 url = 'https://warwidowsnsw.com.au/'
 resp = urlopen(url, context = ctx)
-html_raw = resp.read().decode()
-html = html_raw.replace('&nbsp;',' ')
+#return html in a byte string and decode to unicode string
+html = resp.read().decode()
 
 
-
+def replace_tags(text, tags):
+  for tag in tags:
+    text = text.replace('<' + tag + '>', ' ').replace('</' + tag + '>', ' ')
+    print(tag, 'REPLACED')
+  return text
 
 def tag_visible(element):
     if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]' ]:
@@ -26,13 +30,9 @@ def tag_visible(element):
 def text_from_html(body):
     count = 0
     soup = BeautifulSoup(body, 'html.parser')
-    # return all tags which hold text between opening and closing tag
-    invalid_tags = ['b', 'strong', 'em', 'i']
-    for tag in invalid_tags:
-        for match in soup.find_all(tag):
-            match.unwrap()
+    # return all tag which hold text between opening and closing tag
     texts = soup.find_all(text=True)
-        # filer out all tags which are not visible on the webpage, returns filer object which is iterable
+    # filer out all tags which are not visible on the webpage, returns filer object which is iterable
     visible_texts = filter(tag_visible, texts)
     for t in visible_texts:
         sentences = t.split('. ')
@@ -44,6 +44,8 @@ def text_from_html(body):
     print(count, "sentences")
     #return u"\n".join(t.strip()
 
+invalid_tags = ['b', 'strong', 'em', 'i']
+html = replace_tags(html, invalid_tags)
 text = text_from_html(html)
 
 # b/strong/i/em tags
