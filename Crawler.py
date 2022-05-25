baa695aa90a5b92b7992fb8213dc09ddfa1460f5
nvando <nikievandorst@gmail.com>
2022-05-25 16:32:10 +0200

    Replace hardcoded values with interactive input
    Also:
     - removed duplicated code
     - improved speed word_finder.py by using html from pages table instead of retrieving URL
     - clarified comments

---
+++ Crawler.py
@@ -0,0 +1,159 @@
+from urllib.request import urlopen
+from urllib.parse import urljoin
+from urllib.parse import urlparse
+import ssl
+from bs4 import BeautifulSoup
+import sqlite3
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+
+# create table which includes all pagelinks with html
+conn = sqlite3.connect("Crawler.sqlite3")
+cur = conn.cursor()
+
+cur.execute(
+    """CREATE TABLE IF NOT EXISTS Pages(
+id INTEGER PRIMARY KEY,
+url TEXT UNIQUE,
+sentences INTEGER,
+html TEXT,
+error INTEGER
+)"""
+)
+
+
+# check to see if crawler.py has been run before and retrieved pages or if we need to start a new crawl
+cur.execute("""SELECT url FROM Pages WHERE id = 1""")
+row = cur.fetchone()
+if row is not None:
+    starturl = row[0]
+    print(
+        f"Restarting existing crawl of {starturl}. If you want to start a fresh crawl, quit program and delete Pages database."
+    )
+
+# starting new crawl
+# ask for url and check if it is valid
+else:
+    starturl = None
+    while starturl is None:
+        input_url = input("Enter the full website's url: ")
+        parsed_url = urlparse(input_url)
+        if not bool(parsed_url.scheme):
+            print(
+                "Not a valid url, enter the complete url including the 'http://' or 'https://' component: "
+            )
+            continue
+        elif input_url.endswith("/"):
+            starturl = input_url[:-1]
+        elif input_url.endswith("htm") or input_url.endswith("html"):
+            pos = input_url.find("/")
+            starturl = input_url[:pos]
+        else:
+            starturl = input_url
+
+    # insert the website's url in database
+    cur.execute(
+        """INSERT OR IGNORE INTO Pages (url, html, error) VALUES (?, NULL, NULL)""",
+        (starturl,),
+    )
+    conn.commit()
+
+
+# loop through a predefined number of urls on the website
+number_urls = 0
+while True:
+    if number_urls < 1:
+        sval = input("Enter number of pages to retrieve, or press enter to quit: ")
+        # break out of the loop with enter
+        if len(sval) < 1 or int(sval) == 0:
+            break
+        number_urls = int(sval)
+    number_urls = number_urls - 1
+
+    cur.execute(
+        """SELECT id, url FROM Pages WHERE html is NULL and error is NULL ORDER BY RANDOM() LIMIT 1"""
+    )
+    try:
+        row = cur.fetchone()
+        id = row[0]
+        url = row[1]
+        print(url, " ")
+
+    except:
+        print("No unretrieved HTML pages found")
+        number_urls = 0
+        break
+
+    # returns html document in a single big string (incl. new lines), BS decodes from bytes (likely UTF-8)
+    try:
+        resp = urlopen(url, context=ctx)
+        html = resp.read()
+
+        if resp.getcode() != 200:
+            print("Error on page: ", html.getcode())
+            cur.execute("""UPDATE Pages SET error = ? WHERE url = ?""", (html.getcode(), url))
+
+        # delete url from Pages if it links to non text/html page
+        if "text/html" != resp.info().get_content_type():
+            print("non text/html page - ignored")
+            cur.execute("""DELETE FROM Pages WHERE url = ?""", (url,))
+            conn.commit()
+            continue
+
+        # parse html with BeautifulSoup
+        soup = BeautifulSoup(html, "html.parser")
+
+    except KeyboardInterrupt:
+        print("\n Program interupted by user")
+        break
+    except:
+        print("Unable to retrieve or parse page")
+        cur.execute("""UPDATE Pages SET error = -1 WHERE url = ?""", (url,))
+        conn.commit()
+        continue
+
+    cur.execute("""UPDATE Pages SET html = ? WHERE url = ?""", (html, url))
+    # conn.commit()
+
+    # Retrieve all of the anchor tags (objects) within the html and
+    # add each internal link to the Pages database, if it is not already in there
+    count = 0
+    tags = soup("a")
+    for tag in tags:
+        href = tag.get("href", None)
+        if href is None:
+            continue
+        if len(href) < 1:
+            continue
+        if href.endswith("/"):
+            href = href[:-1]
+        # ignore links to images
+        if (
+            href.endswith(".png")
+            or href.endswith(".jpeg")
+            or href.endswith(".gif")
+            or href.endswith(".jpg")
+            or href.endswith(".pdf")
+        ):
+            continue
+        # resolve relative links like href="/contact"
+        if href.startswith("/"):
+            parsed = urlparse(href)
+            if len(parsed.scheme) < 1:
+                href = urljoin(url, href)
+        # ignore external links
+        if not href.startswith(starturl):
+            continue
+
+        cur.execute(
+            """INSERT OR IGNORE INTO Pages (url, html, error) VALUES (?, NULL, NULL)""", (href,)
+        )
+        count = count + 1
+        conn.commit()
+
+    print(count, "links")
+cur.close()
--- wordfinder.py
+++ wordfinder.py
@@ -1,132 +1,151 @@
 from urllib.request import urlopen
+from urllib.parse import urlparse
 import re
 import ssl
 from bs4 import BeautifulSoup
 import sqlite3
 
 
+body_tags = ["h1", "h2", "h3", "h4", "p", "li", "lu", "span"]
+
+
+def extract_sentences_body(soup, tags, target_word):
+    #  extract sentences which include the target word
+    #  from html body text (text within 'p', 'h1' to 'h4', 'li', and 'span' tags)
+    #  add the matched sentences into the Matches table
+    #  count the number of sentences with matches
+    global sent_count
+    for tag in tags:
+        for t in soup.findAll(tag):
+            tag_text = t.get_text()
+            # split sentences based after a dot followed
+            # by one or multipe white spaces,
+            # or after an exlemation or question mark.
+            # '?:' means a non-capturing group , 's+' matches a string of whitespace characters
+            sentences = re.split("(?:\.\s+|\!|\?)", tag_text)
+
+            for s in sentences:
+                # search each sentence for a match with the target word, ignore case
+                if target_word in s.lower():  # CHANGE TO TARGET WORD, ASK FOR WITH INPUT
+                    cur.execute(
+                        """INSERT INTO Matches (page_id, path, sentence) VALUES (?, ?, ?)""",
+                        (page_id, url_path, s),
+                    )
+                    conn.commit()
+                    print(f"Matched within {tag} tag: {s}")
+                    print("--------------")
+                    sent_count = sent_count + 1
+
+
+def extract_sentences_repeated_blocks(block, target_word):
+    #  extract sentences containing target word
+    #  from the footer or menu items within the header
+    #  these blocks are repeated on every page
+    #  but the matched sentences are added into the database only once
+    #  with url = 'header' or 'footer' and page_id = 'NA'
+
+    # first make sure header/footer has been searched already
+    cur.execute("SELECT * FROM pages WHERE url = ?", (block,))
+    if cur.fetchone() is None:
+        # select a row from pages which contains html. Header/footer is repeated on
+        # every page, so it doesn't matter which page is chosen
+        cur.execute("""SELECT html FROM Pages WHERE html is not NULL ORDER BY RANDOM() LIMIT 1""")
+        row = cur.fetchone()
+        html = row[0]
+
+        # parse html with BeautifulSoup
+        soup = BeautifulSoup(html, "html.parser")
+
+        # extract header/footer from parsed html
+        extracted_block = soup.find(block).extract()
+        text = extracted_block.get_text(separator="\n")
+        sentences = re.split("\n", text)
+        count = 0
+        print(f"searching {block} ")
+        for s in sentences:
+            if target_word in s.lower():
+                cur.execute(
+                    """INSERT INTO Matches (page_id, path, sentence) VALUES ('NA', ?, ?)""",
+                    (block, s),
+                )
+                conn.commit()
+                print(f"""Found match in {block}. '{s}' added to database.""")
+                count = count + 1
+        print(f"sentences in {block} containing the search word: {count}")
+        cur.execute(
+            """INSERT INTO Pages (url, sentences, html) VALUES (?,? , 'NA')""",
+            (
+                block,
+                count,
+            ),
+        )
+        conn.commit()
+
+    else:
+        print(f"The {block} has already been checked for matches")
+
+
+#########################################################################################################
+
+
+# Connect to Crawler database and create 'Matches' table
+conn = sqlite3.connect("Crawler.sqlite3")
+cur = conn.cursor()
+cur.execute(
+    """CREATE TABLE IF NOT EXISTS Matches(
+id INTEGER PRIMARY KEY,
+page_id INTEGER,
+path VARCHAR,
+sentence VARCHAR
+)"""
+)
+
 # Ignore SSL certificate errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-
-conn = sqlite3.connect("KBCS")
-cur = conn.cursor()
-
-cur.execute(
-    """CREATE TABLE IF NOT EXISTS PageMatches(
-id INTEGER PRIMARY KEY,
-page_id INTEGER,
-url VARCHAR,
-sentence VARCHAR
-)"""
-)
+target_word = input("Enter the word(s) you want to search the site for: ")
+print(f"""The website will be searched for sentences which include the word(s) '{target_word}'.""")
 
 
-def extract_sentences(text, tags):
-    # extract sentences which include the target word
-    # from html body text (text within 'p', 'h1' and 'h2' tags)
-    # add sentences which match in Pagematches database
-    # and count the number of sentences with matches
-    global sent_count
-    for tag in tags:
-        print("tag = ", tag)
-        for tag in text.findAll(tag):
-            tag_text = tag.get_text()
-            sentences = re.split("(?:\.\s+|\!|\?)", tag_text)
-            for s in sentences:
-                # search each sentence for a match with the target word, ignore case
-                if "kbcs" in s.lower():
-                    cur.execute(
-                        """INSERT INTO PageMatches (page_id, url, sentence) VALUES (?, ?, ?)""",
-                        (page_id, url_short, s),
-                    )
-                    conn.commit()
-                    print("--------------")
-                    print(s)
-                    sent_count = sent_count + 1
+# find target word within menu items of header and add to Matches
+extract_sentences_repeated_blocks("header", target_word)
+# check if menu items within the footer contain the target word
+extract_sentences_repeated_blocks("footer", target_word)
 
 
-body_tags = ["h1", "h2", "h3", "h4", "p", "li", "span"]
-
+# check if menu items within the footer contain the target word
 while True:
-    # select random unretrieved url from Pagelinks database
+    # select random unretrieved url from the Pages database
+    # check for macthes in the html of the body
+    # add matched senteces to Pages
     cur.execute(
-        """SELECT id, url FROM Pagelinks WHERE sentences is NULL and html is not NULL and error is NULL ORDER BY RANDOM() LIMIT 1"""
+        """SELECT id, url, html FROM Pages WHERE sentences is NULL and html is not NULL and error is NULL ORDER BY RANDOM() LIMIT 1"""
     )
     try:
         row = cur.fetchone()
         page_id = row[0]
         url = row[1]
-        print(url)
+        html = row[2]
+        print("searching url: ", url)
     except:
         print("No unretrieved pages found in database")
-        quit()
+        break
 
-    # retrieve root-relative from absolute url
-    if url.endswith("sydneybats.org.au"):
-        url_short = url
-    else:
-        url_short = url.split(".au", 1)[1]
+    # retrieve url path from absolute url
 
-    # open url and retrieve html, in a byte string
-    # decode to unicode string
-    # open with beautiful soup
-    resp = urlopen(url, context=ctx)
-    html = resp.read().decode()
+    url_path = urlparse(url).path
+
+    # parse html with beautiful soup
     soup = BeautifulSoup(html, "html.parser")
-    # extract header tag as menu items will be extracted and stored in the database differenlty.
-    # all menu items containing the search word will be added to PageMatches as recurrent
 
-    # extract header and custom footer tag for later use
-    #  as the text within these tags are the same on every webpage these only need to be added to Pagematches once
-    header = soup.find("header").extract()
-    footer = soup.find("div", id="custom_footer").extract()
-
-    # now extract and count number of sentences  containing the target word within one webpage
+    # extract and count number of sentences containing the target word within one webpage
     sent_count = 0
-    body_matches = extract_sentences(soup, body_tags)
-    print(sent_count, "sentences with match")
-    cur.execute("""UPDATE Pagelinks SET sentences = ? WHERE url = ?""", (sent_count, url))
+    body_matches = extract_sentences_body(soup, body_tags, target_word)
+    print(f"Finished searching this page. Found {sent_count} sentences with match")
+    cur.execute("""UPDATE Pages SET sentences = ? WHERE url = ?""", (sent_count, url))
     conn.commit()
 
-
-# add all menu items within the header containing the target word to PageMatches as recurrent
-header_text = header.get_text(separator="\n")
-sentences = re.split("\n", header_text)
-head_count = 0
-for s in sentences:
-    if "kbcs" in s.lower():
-        cur.execute(
-            """INSERT INTO PageMatches (page_id, url, sentence) VALUES ('NA', 'Menu', ?)""", (s,)
-        )
-        conn.commit()
-        print("menu item: ", s, "added to database as recurrent")
-        head_count = head_count + 1
-print("sentences in footer containing the search word: ", fcount)
-cur.execute(
-    """INSERT INTO Pagelinks (url, sentences, html) VALUES ('Menu',? , 'NA')""", (head_count,)
-)
-conn.commit()
-
-
-# add text within custom footer containing the target word to PageMatches as recurrent
-footer_text = footer.get_text(separator="\n")
-sentences = re.split("\n", footer_text)
-foot_count = 0
-for s in sentences:
-    if "kbcs" in s.lower():
-        cur.execute(
-            """INSERT INTO PageMatches (page_id, url, sentence) VALUES ('NA', 'Footer', ?)""", (s,)
-        )
-        conn.commit()
-        print("custom footer: ", s, "added to database as recurrent")
-        foot_count = foot_count + 1
-print("sentences in footer containing the search word: ", foot_count)
-cur.execute(
-    """INSERT INTO Pagelinks (url, sentences, html) VALUES ('Footer',? , 'NA')""", (foot_count,)
-)
-conn.commit()
 
 cur.close()
